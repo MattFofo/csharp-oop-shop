b3f686930c3f1a4a35502c0c9ea0894c9a14d2c3
MattFofo <matteo.segattini2@gmail.com>
2022-06-29 14:48:49 +0200

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Product
+    {
+        private int Codice { get; }
+        public string nome;
+        public string descrizione;
+        public double prezzo;
+        public double iva;
+
+        public Product(string name, double price, string description, int iva)
+        {
+            this.nome = name;
+            this.prezzo = price;
+            this.descrizione = description;
+            this.iva = iva;
+            Random rand = new Random();
+            this.Codice = rand.Next(1, 99999999);
+        }
+
+        public double GetBasicPrice()
+        {
+            return this.prezzo;
+        }
+
+        public double GetPriceAfterIva()
+        {
+            double ivaPriced = Math.Round((this.prezzo + (this.prezzo / 100 * this.iva)), 2);
+
+            return ivaPriced;
+        }
+
+        public string GetFullName()
+        {
+            string fullName = this.nome + " " + "codice: " + this.Codice;
+
+            return fullName;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using csharp_oop_shop;
+
+Product myProduct = new Product("libro", 18.88, "un bel libro", 30);
+
+Console.WriteLine(myProduct.nome);
+Console.WriteLine(myProduct.prezzo);
+Console.WriteLine(myProduct.descrizione);
+
+Console.WriteLine(myProduct.GetBasicPrice());
+Console.WriteLine(myProduct.GetPriceAfterIva());
